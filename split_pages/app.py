15ee82d5f8949e9a12815521bd3e07b7b2345409
Michael Corey <corey101@umn.edu>
2023-01-17 14:05:56 -0600

    moar splitting

--- split_pages/app.py
+++ split_pages/app.py
@@ -1,5 +1,6 @@
 import io
 import re
+import math
 import urllib.parse
 import boto3
 
@@ -33,10 +34,12 @@ covenants-deeds-images
 Image.MAX_IMAGE_PIXELS = 1000000000
 s3 = boto3.client('s3')
 
-def check_oversized(bucket, key, im):
+def check_oversized_dimen(bucket, key, im):
     '''
     Test pixel dimensions to make sure Textract won't reject it.
     Max dimensions for Textract are 10,000 pixels in either dimension
+
+    Note: This is separate from MEMORY max size, which is covered separately
     '''
 
     width, height = im.size
@@ -57,6 +60,7 @@ def check_oversized(bucket, key, im):
                         * float(new_height/float(height)))
 
     im = im.resize((new_width, new_height), Image.ANTIALIAS)
+
     buffer = io.BytesIO()
     im.save(buffer, format="tiff")
     buffer.seek(0)
@@ -72,6 +76,41 @@ def check_oversized(bucket, key, im):
     )
     return True
 
+
+def check_oversized_mem(im, max_bytes=10485760):
+    '''
+    Check if nbytes is small enough for textract, which has a limit of 10485760 bytes
+
+    returns:
+        Value 1, bool: Was the image oversized, requiring resave?
+        Value 2, im: PIL image object
+    '''
+    buffer = io.BytesIO()
+    im.save(buffer, format="tiff")
+    byte_size = buffer.getbuffer().nbytes
+
+    if byte_size > max_bytes:
+        print(f'Memory resize needed. Original size: {byte_size}. Reduction needed')
+
+        # Adapted from https://stackoverflow.com/questions/66455731/how-to-calculate-the-resulting-filesize-of-image-resize-in-pil
+        original_bytes_per_pixel = byte_size / (im.size[0] * im.size[1])
+
+        new_bytes_per_pixel = original_bytes_per_pixel * (max_bytes / byte_size)
+        new_bytes_ratio = math.sqrt(new_bytes_per_pixel / original_bytes_per_pixel)
+        new_width, new_height = int(0.95* new_bytes_ratio * im.size[0]), int(0.95* new_bytes_ratio * im.size[1])
+
+        im = im.resize((new_width, new_height), Image.ANTIALIAS)
+
+        buffer = io.BytesIO()
+        im.save(buffer, format="tiff")
+        final_byte_size = buffer.getbuffer().nbytes
+        print(f'Resized to {final_byte_size} bytes ({round(final_byte_size / max_bytes, 2)}% of max)')
+        return True, im
+    else:
+        print('Memory within limits.')
+        return False, im
+
+
 def split_tiff(bucket, key, im):
     num_pages = im.n_frames
 
@@ -81,17 +120,15 @@ def split_tiff(bucket, key, im):
     else:
         print(f"{num_pages} pages")
         print(im.mode)
-#         if im.mode == 'L':
-#             try:
-#                 im = im.convert('RGB')
-#             except:
-#                 arr = np.array(im)
-#                 im = Image.fromarray(arr)
-#                 im.save('test_images/test_convert.tif', format="tiff")
+
         page_buffers = []
         for page_num in range(0, num_pages):
             im.seek(page_num)
             page_im = im.copy()
+
+            # Check memory size...
+            mem_too_big, page_im = check_oversized_mem(page_im)
+
             buffer = io.BytesIO()
             page_im.save(buffer, format="tiff")
             buffer.seek(0)
@@ -121,25 +158,62 @@ def lambda_handler(event, context):
             }
         }
 
+    if ".DS_Store" in key:
+        return {
+            "statusCode": 200,
+            "body": {
+                "message": ".DS_Store file. Ignore!",
+                "pages": []
+            }
+        }
+
     response = s3.get_object(Bucket=bucket, Key=key)
 
     im = Image.open(response['Body'])
 
-    bool_oversized = check_oversized(bucket, key, im)
-    if bool_oversized:
-        print('Oversized TIF. Will try again.')
+    bool_oversized_dimen = check_oversized_dimen(bucket, key, im)
+    if bool_oversized_dimen:
+        print('Oversized TIF (dimensions). Will try again.')
         return {
             "statusCode": 200,
             "body": {
-                "message": "Oversized image detected. A resized copy has been written over the raw file, which should trigger another Step Function.",
+                "message": "Oversized image detected (dimensions). A resized copy has been written over the raw file, which should trigger another Step Function.",
                 "pages": []
             }
         }
 
     split_result = split_tiff(bucket, key, im)
     if len(split_result) == 1:
-        print('Single page TIFF, pass to next step as is...')
-        out_keys = split_result
+        print('Single page TIFF, checking memory size...')
+
+        # Check memory size...
+        mem_too_big, im = check_oversized_mem(im)
+        if mem_too_big:
+            buffer = io.BytesIO()
+            im.save(buffer, format="tiff")
+            buffer.seek(0)
+
+            # Overwrite image in raw bucket if resized.
+            s3.put_object(
+                Body=buffer,
+                Bucket=bucket,
+                Key=key,
+                StorageClass='GLACIER_IR',
+                ContentType='image/tif',
+                # ACL='public-read'
+            )
+
+            # Now exit, because save will trigger new run.
+            print('Oversized TIF (memory). Will try again.')
+            return {
+                "statusCode": 200,
+                "body": {
+                    "message": "Oversized image detected (dimensions). A resized copy has been written over the raw file, which should trigger another Step Function.",
+                    "pages": []
+                }
+            }
+        else:
+            out_keys = split_result
 
     elif len(split_result) > 1:
         print(f'Multi-page TIF split into {len(split_result)} pages.')
@@ -151,7 +225,7 @@ def lambda_handler(event, context):
 
             out_keys.append({'bucket': page['bucket'], 'key': out_key, 'page_num': page['page_num']})
 
-            # Upload image page to destination bucket
+            # Upload split image page to raw bucket
             s3.put_object(
                 Body=page['img_buffer'],
                 Bucket=bucket,
